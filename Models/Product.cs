a0b11348c3ede9c4c029a062973761f34927952e
Irina Vlad <irina.vlad99@gmail.com>
2023-01-16 01:23:01 +0200

    Pana la behavior

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SQLiteNetExtensions.Attributes;
+
+namespace TrainingStudioApp.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+    }
+}
